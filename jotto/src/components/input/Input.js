caafe574a1a9998d0938b3cfb263e66a65faa643
Ash Blue <ash@blueashes.com>
2019-09-07 18:01:51 -0600

    New input component failure state

--- jotto/src/components/input/Input.js
+++ jotto/src/components/input/Input.js
@@ -1,20 +1,41 @@
 import React, {Component} from 'react';
-import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 
 class Input extends Component {
+
   render() {
+    const {success} = this.props;
+
+    let contents = null;
+    if (!success) {
+      contents = (
+        <form className="form-inline">
+          <input
+            type="text"
+            data-test="input-box"
+            className="mb-2 mx-sm-3"
+            placeholder="enter guess"
+          />
+          <button
+            data-test="button-submit"
+            type="submit"
+            className="btn btn-primary mb-2">
+            Submit
+          </button>
+        </form>
+      );
+    }
+
     return (
-      <div>
-        <button></button>
+      <div data-test="input">
+        {contents}
       </div>
     );
   }
 }
 
-// Input.propTypes = {};
-
-const mapStateToProps = (state) => ({
+const mapStateToProps = ({success}) => ({
+  success,
 });
 
 export default connect(mapStateToProps)(Input);
--- jotto/src/components/input/Input.test.js
+++ jotto/src/components/input/Input.test.js
@@ -4,6 +4,7 @@ import Input from "./Input";
 import {shallow} from 'enzyme';
 import reducers from '../../../src/reducers';
 import {createStore} from "redux";
+import {findByTestAttr} from "../../../test/testUtils";
 
 export const storeFactory = (initialState) => {
   return createStore(reducers, initialState);
@@ -11,39 +12,54 @@ export const storeFactory = (initialState) => {
 
 const setup = (initialState={}) => {
   const store = storeFactory(initialState);
-  const wrapper = shallow(<Input store={store} />).dive().dive();
-  console.log(wrapper.debug());
+  return shallow(<Input store={store} />).dive().dive();
 };
-
-// const wrapper = shallow(<Input store={store} />).dive().dive();
 
 describe('render', () => {
   describe('word has not been guessed', () => {
-    it('renders component without error', () => {
-      setup();
+    let wrapper;
+    beforeEach(() => {
+      const initialState = {success: false};
+      wrapper = setup(initialState);
+    });
+
+    it('renders component', () => {
+      const component = findByTestAttr(wrapper, 'input');
+      expect(component.length).toBe(1);
     });
 
     it('renders component input box', () => {
-
+      const inputBox = findByTestAttr(wrapper, 'input-box');
+      expect(inputBox.length).toBe(1);
     });
 
     it('renders component button', () => {
-
+      const submitButton = findByTestAttr(wrapper, 'button-submit');
+      expect(submitButton.length).toBe(1);
     });
   });
 
   describe('word has been guessed', () => {
-    describe('does not', () => {
-      it('renders component without error', () => {
+    let wrapper;
+    beforeEach(() => {
+      const initialState = {success: true};
+      wrapper = setup(initialState);
+    });
 
+    describe('does not', () => {
+      it('renders component', () => {
+        const component = findByTestAttr(wrapper, 'input');
+        expect(component.length).toBe(1);
       });
 
       it('renders component input box', () => {
-
+        const inputBox = findByTestAttr(wrapper, 'input-box');
+        expect(inputBox.length).toBe(0);
       });
 
       it('renders component button', () => {
-
+        const inputBox = findByTestAttr(wrapper, 'button-submit');
+        expect(inputBox.length).toBe(0);
       });
     });
   });
