d4998451c209ffbd83643ae074a11e2be2376626
Ash Blue <ash@blueashes.com>
2019-09-08 14:28:18 -0600

    Added mapping props to the app

--- jotto/src/App.js
+++ jotto/src/App.js
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 import './App.css';
 import Congrats from "./components/congrats/Congrats";
 import GuessedWords from "./components/guessedWords/GuessedWords";
+import {connect} from "react-redux";
+import {getSecretWord} from "./actions";
+import Input from "./components/input/Input";
 
-function App() {
-  return (
-    <div className="container">
-      <h1>Jotto</h1>
-      <Congrats success={true}/>
-      <GuessedWords guessedWords={[
-        {guessedWord: 'train', letterMatchCount: 3}
-      ]}/>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>Jotto</h1>
+        <Congrats success={this.props.success}/>
+        <Input />
+        <GuessedWords guessedWords={this.props.guessedWords}/>
+      </div>
+    );
+  }
 }
 
-export default App;
+App.propTypes = {
+  success: PropTypes.bool.isRequired,
+  guessedWords: PropTypes.array.isRequired,
+  secretWord: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = ({success, guessedWords, secretWord}) => ({
+  success,
+  guessedWords,
+  secretWord,
+});
+
+export default connect(mapStateToProps, {getSecretWord})(App);
--- jotto/src/App.test.js
+++ jotto/src/App.test.js
@@ -1,9 +1,51 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import {shallow} from 'enzyme';
+import {storeFactory} from "../test/testUtils";
+
+const setup = (initialState = {}) => {
+  const defaultState = {secretWord: 'a', guessedWords: [], success: false};
+  const store = storeFactory({...defaultState, ...initialState});
+  return shallow(<App store={store} />).dive().dive();
+};
+
+it('renders the element', () => {
+  const app = setup();
+  expect(app).toBeTruthy();
+});
+
+describe('redux properties', () => {
+  it('has success state prop', () => {
+    const success = true;
+    const wrapper = setup({success});
+    const successProp = wrapper.instance().props.success;
+
+    expect(successProp).toBe(success);
+  });
+
+  it('has guessedWords state prop', () => {
+    const guessedWords = [{
+      guessedWord: 'mom',
+      letterMatchCount: 2,
+    }];
+    const wrapper = setup({guessedWords});
+    const guessedWordsProp = wrapper.instance().props.guessedWords;
+
+    expect(guessedWordsProp).toBe(guessedWords);
+  });
+
+  it('has secretWord state prop', () => {
+    const secretWord = 'party';
+    const wrapper = setup({secretWord});
+    const secretWordProp = wrapper.instance().props.secretWord;
+
+    expect(secretWordProp).toBe(secretWord);
+  });
+
+  it('has getSecretWord action function prop', () => {
+    const wrapper = setup();
+    const getSecretWordProp = wrapper.instance().props.getSecretWord;
+    expect(getSecretWordProp).toBeInstanceOf(Function);
+  });
 });
